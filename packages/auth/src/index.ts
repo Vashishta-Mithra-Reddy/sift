c7a9a940d01a1ccf56de4126fb361415ba71271d
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2026-01-16 19:07:35 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@sift/db";
+import * as schema from "@sift/db/schema/auth";
+import { env } from "@sift/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});

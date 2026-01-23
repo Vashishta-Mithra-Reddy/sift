5c80504716f732c5b4e5423c6950ea05dda0dda5
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2026-01-23 18:10:25 +0530

    favicons and such

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -4,8 +4,8 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "sift",
     short_name: "sift",
-    description: "my pwa app",
-    start_url: "/new",
+    description: "Sift is a platform that helps you recall information more effectively. Simply put it creates quizes and helps you master the information you learn.",
+    start_url: "/sifts",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
